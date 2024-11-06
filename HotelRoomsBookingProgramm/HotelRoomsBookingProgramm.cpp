d00cca4fbb15853134cdd7258bb1a883a662f631
Dmitry <diman567tak@gmail.com>
2024-11-06 12:11:09 +0200

    Made a version of a booking program for one point

--- HotelRoomsBookingProgramm/HotelRoomsBookingProgramm.cpp
+++ HotelRoomsBookingProgramm/HotelRoomsBookingProgramm.cpp
@@ -1,20 +1,111 @@
-// HotelRoomsBookingProgramm.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
 
 #include <iostream>
+#include <string>
+
+int generateRoom() {
+    return rand() % 2;
+}
+
+void generateRandomBookedRooms(unsigned short int rooms[],
+    unsigned short int numberOfRooms) {
+    for (int i = 0; i < numberOfRooms; i++)
+    {
+        rooms[i] = generateRoom();
+    }
+}
+
+bool isRoomAwailable(unsigned short int roomNumber,
+    unsigned short int room[]) {
+    return room[roomNumber] == 0;
+}
+
+int getInputAsInt(std::string& userInput) {
+    unsigned short int userInputNumber = 0;
+
+    for (char letter : userInput) {
+        userInputNumber = userInputNumber * 10 + (letter - '0');
+    }
+    return userInputNumber;
+}
+
+bool checkIsInputValid(std::string& userInput, short int numberOfRooms) {
+    unsigned short int length = 0;
+    
+    for (char symbol : userInput) {
+        if (!std::isdigit(symbol))
+            return false;
+
+        length++;
+    }
+
+    if (getInputAsInt(userInput) > numberOfRooms)
+        return false;
+
+    return true;
+}
+
+std::string getUserInput(unsigned short int numberOfRooms) {
+    std::string userInput;
+
+    std::cin >> userInput;
+
+    if (!checkIsInputValid(userInput, numberOfRooms))
+    {
+        do {
+            std::cout << "Your input is wrong, please write another number: ";
+            std::cin >> userInput;
+        } while (!checkIsInputValid(userInput, numberOfRooms));
+    }
+
+    return userInput;
+}
+
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    using namespace std;
+
+    const unsigned int numberOfRooms = 10;
+
+    //0 - free 1 - booked
+    unsigned short int rooms[numberOfRooms];
+
+    unsigned int totalBill = 0;
+
+    string userInput;
+
+    generateRandomBookedRooms(rooms, numberOfRooms);
+
+    cout << "Welcome to hotel booking programm!" << endl;
+    cout << "Price of a room is 100e per night" << endl;
+    cout << "Currently we have " << numberOfRooms << " rooms" << endl;
+
+    cout << "Write a number of a room you want to book: ";
+
+    while (true) {
+
+        userInput = getUserInput(numberOfRooms);
+
+        if (getInputAsInt(userInput) == 0)
+            break;
+
+        if((isRoomAwailable(getInputAsInt(userInput), rooms))) {
+            cout << "Room is awailable!" << endl;
+            cout << "For how many nights you want to book it? " << endl;
+            userInput = getUserInput(numberOfRooms);
+
+            rooms[getInputAsInt(userInput)] = 1;
+
+            totalBill += getInputAsInt(userInput) * 100;
+
+            cout << "Total amount of the bill is: " << totalBill << "e" << endl;
+
+            cout << "Write 0 to exit or number of another room, that you want to book ";
+        }
+        else {
+            cout << "Unfortunately room is unawailable, please choose another number " << endl;
+            cout << "Write another number of a room you want to book: ";
+        }
+    }
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
