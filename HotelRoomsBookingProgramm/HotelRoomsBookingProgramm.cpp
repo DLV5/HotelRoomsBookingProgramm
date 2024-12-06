295b5aeefe98639effd502de20614e720cadda8e
Kameji <diman567tak@gmail.com>
2024-12-06 17:05:10 +0200

    Added stuct "reservations" instead of three sepparate vectors. Also made 4 global variables for max/min rooms and room prices

--- HotelRoomsBookingProgramm/HotelRoomsBookingProgramm.cpp
+++ HotelRoomsBookingProgramm/HotelRoomsBookingProgramm.cpp
@@ -2,19 +2,30 @@
 #include <iostream>
 #include <string>
 
-void greetTheUser(unsigned short int singleRoomPrice, unsigned short int doubleRoomPrice,
-    unsigned short int currentNumberOfRooms) {
+struct reservation
+{
+    int id;
+    std::string name;
+    std::vector<int> bookedRoomNumbers;
+};
+
+const unsigned int minNumberOfRooms = 40;
+const unsigned int maxNumberOfRooms = 300;
+
+const unsigned int singleRoomPrice = 100;
+const unsigned int doubleRoomPrice = 150;
+
+void greetTheUser(unsigned short int currentNumberOfRooms) {
     using namespace std;
     cout << "Welcome to hotel booking programm!" << endl;
     cout << "Price of a single room is " << singleRoomPrice << "e per night" << endl;
     cout << "Price of a double room is " << doubleRoomPrice << "e per night" << endl;
     cout << "Currently we have " << currentNumberOfRooms << " rooms" << endl;
     cout << "-------------------------------------------" << endl;
-    cout << "Press 1 to book a room or 2 to check your reservation, press 0 to exit ";
+    cout << "Press 1 to book a room or 2 to check your reservation, press 0 to exit " << endl;
 }
 
-unsigned short int generateNumberOfRooms(unsigned short int minNumberOfRooms,
-    unsigned short int maxNumberOfRooms) {
+unsigned short int generateNumberOfRooms() {
     srand(time(NULL));
     return rand() % (maxNumberOfRooms - minNumberOfRooms) + minNumberOfRooms;
 }
@@ -117,10 +128,12 @@ std::string getUserInput() {
     return userInput;
 }
 
-void getEntireStringAsUserInput(std::string& input) {
+std::string getEntireStringAsUserInput(std::string input) {
     std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
 
     std::getline(std::cin, input); 
+
+    return input;
 }
 
 float getDiscount() {
@@ -173,32 +186,30 @@ void bookTheRoom(std::vector<int>& currentUserBookedRooms,
     cout << "Write 0 to finish reservation or number of another room, that you want to book ";
 }
 
-void finishReservation(std::vector<std::string>& reservationNames, 
-    std::vector<int>& reservationNumbers, std::vector<int> currentUserBookedRooms,
-    std::vector<std::vector<int>>& bookedRooms) {
+void finishReservation(std::vector<reservation>& reservations, std::vector<int> currentUserBookedRooms) {
     using namespace std;
 
     cout << "For finishing reservation please write your name: " << endl;
 
-    string input;
+    string reservationName;
     int reservaitonNumber;
-
-    getEntireStringAsUserInput(input);
+    reservation userReservation;
 
     reservaitonNumber = getReservationNumber();
+    reservationName = getEntireStringAsUserInput(reservationName);
 
-    reservationNames.push_back(input);
-    reservationNumbers.push_back(reservaitonNumber);
+    userReservation.id = reservaitonNumber;
+    userReservation.name = reservationName;
+    userReservation.bookedRoomNumbers = currentUserBookedRooms;
 
-    cout << "Thank you for your reservation, " << input << endl;
+    reservations.push_back(userReservation);
+
+    cout << "Thank you for your reservation, " << reservationName << endl;
     cout << "Your reservation number is: " << reservaitonNumber << endl;
-
-    bookedRooms.push_back(currentUserBookedRooms);
 }
 
 void processReservation(unsigned short int currentNumberOfRooms, 
-    std::vector<std::string>& reservationNames, std::vector<int>& reservationNumbers,
-    std::vector<std::vector<int>>& bookedRooms, std::vector<int> rooms) {
+    std::vector<reservation>& reservations, std::vector<int> rooms) {
     using namespace std;
 
     string userInput;
@@ -218,10 +229,8 @@ void processReservation(unsigned short int currentNumberOfRooms,
 
         if (userInputAsNumber == 0) {
             finishReservation(
-                reservationNames, 
-                reservationNumbers, 
-                currentUserBookedRooms, 
-                bookedRooms
+                reservations, 
+                currentUserBookedRooms 
             );
             break;
         }
@@ -245,33 +254,43 @@ void processReservation(unsigned short int currentNumberOfRooms,
     cout << "Press 1 to book a room or 2 to check your reservation, press 0 to exit " << endl;
 }
 
-void checkReservation(std::vector<std::vector<int>> bookedRooms, 
-    std::vector<std::string> reservationNames, std::vector<int> reservationNumbers) {
+void checkReservation(std::vector<reservation> reservations) {
     using namespace std;
+
+    bool wasReservationRound = false;
+
     cout << "Please enter your reservation number: ";
 
     while (true) {
         string userInput = getUserInput();
         int userInputAsNumber = getInputAsInt(userInput);
 
-        auto num = find(reservationNumbers.begin(), reservationNumbers.end(), getInputAsInt(userInput));
-        int index = distance(reservationNumbers.begin(), num);
+        for (reservation userReservation : reservations)
+        {
+            if (userReservation.id = getInputAsInt(userInput))
+            {
+                cout << "Reservation was found! " << endl;
+                cout << "Name: " << userReservation.name << endl;
+                cout << "Booked room(s): ";
+                for (int room : userReservation.bookedRoomNumbers)
+                {
+                    cout << room << " ";
+                }
+                cout << endl;
 
-        if (num != reservationNumbers.end()) {
-            cout << "Reservation was found! " << endl;
-            cout << "Name: " << reservationNames[index] << endl;
-            cout << "Booked rooms: ";
-            for (int room : bookedRooms[index]) {
-                cout << room << " ";
+                wasReservationRound = true;
             }
-            cout << endl;
         }
-        else if (userInputAsNumber == 0) {
-            break;
-        }
-        else {
+
+        if (!wasReservationRound)
+        {
             cout << "Can't find reservation number, please enter another number " << endl;
-        } 
+
+            if (userInputAsNumber == 0)
+            {
+                break;
+            }
+        }
     }
 
     cout << "Press 1 to book a room or 2 to check your reservation, press 0 to exit " << endl;
@@ -280,12 +299,6 @@ void checkReservation(std::vector<std::vector<int>> bookedRooms,
 int main()
 {
     using namespace std;
-
-    const unsigned int minNumberOfRooms = 40;
-    const unsigned int maxNumberOfRooms = 300;
-
-    const unsigned int singleRoomPrice = 100;
-    const unsigned int doubleRoomPrice = 150;
 
     unsigned short int currentNumberOfRooms;
 
@@ -296,13 +309,15 @@ int main()
     vector<string> reservationNames;
     vector<vector<int>> bookedRooms;
 
+    vector<reservation> reservations;
+
     string userInput;
 
-    currentNumberOfRooms = generateNumberOfRooms(minNumberOfRooms, maxNumberOfRooms);
+    currentNumberOfRooms = generateNumberOfRooms();
 
     generateRandomBookedRooms(rooms, currentNumberOfRooms);
 
-    greetTheUser(singleRoomPrice, doubleRoomPrice, currentNumberOfRooms);
+    greetTheUser(currentNumberOfRooms);
 
     while (true) {
 
@@ -318,18 +333,14 @@ int main()
             case 1:
                 processReservation(
                     currentNumberOfRooms,
-                    reservationNames,
-                    reservationNumbers,
-                    bookedRooms,
+                    reservations,
                     rooms);
                 break;
 
             //This part is responsible for cheking reservations
             case 2:
                 checkReservation(
-                    bookedRooms,
-                    reservationNames,
-                    reservationNumbers
+                    reservations
                 );
                 break;
 
